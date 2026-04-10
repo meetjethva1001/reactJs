a6d461c82ffaf954b69921e0fbb3f9feef9479ce
meetJethva <meet@zerotimesolutions.com>
2026-04-10 17:12:08 +0530

    small changes

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export default function useFetch(url: any) {
+export default function useFetch(url: string) {
     const [data, setData] = useState()
     const [loading, setLoading] = useState(false)
 
